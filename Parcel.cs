570a0d76dcb6e4918a74147f48c7112d92edde4e
VladHasiuk <kn1b25.hasiuk@kpnu.edu.ua>
2026-09-13 18:44:52 +0300

    Delete Parcel.cs

--- Parcel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DeliveryApp.Models;
-
-public class Parcel            
-{
-    
-    public string Number { get; set; } = string.Empty;
-    public double Weight { get; set; }
-    public string Recipient { get; set; } = string.Empty;
-}
