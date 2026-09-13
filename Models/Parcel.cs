fe1f7c1ee92b128b7f0138ce10d52afe5a7f6847
VladHasiuk <kn1b25.hasiuk@kpnu.edu.ua>
2026-09-13 18:44:23 +0300

    Add files via upload

---
+++ Models/Parcel.cs
@@ -0,0 +1,9 @@
+﻿namespace DeliveryApp.Models;
+
+public class Parcel            
+{
+    
+    public string Number { get; set; } = string.Empty;
+    public double Weight { get; set; }
+    public string Recipient { get; set; } = string.Empty;
+}
