95aec03261f95be9ab13683ac804b7de2f15d711
VladHasiuk <kn1b25.hasiuk@kpnu.edu.ua>
2026-09-20 18:14:48 +0300

    123

---
+++ Models/Courier.cs
@@ -0,0 +1,24 @@
+namespace DeliveryApp.Models;
+
+public class Courier
+{
+    private string _name = string.Empty;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Ім'я кур'єра не може бути порожнім.");
+            }
+            _name = value.Trim();
+        }
+    }
+
+    public Courier(string name)
+    {
+        Name = name;
+    }
+}
